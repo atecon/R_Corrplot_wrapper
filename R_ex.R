ce0cc15a7aaa839102970ae67dcf312fe1eb8eb4
Artur Tarassow <artur.tarassow@wiso.uni-hamburg.de>
2018-03-14 10:13:58 +0100

    small changes in R example script

--- R_ex.R
+++ R_ex.R
@@ -3,21 +3,20 @@ Corrplot_R <- function(M, method, fname) {
   
   #dev.off()	# avoid permante flashing-up when using gretl
   #png(filename=fname, type="cairo", width=600, height=600, pointsize=14, bg=NA, res=96)
-  corrplot(M, method="color")
+  corrplot(M, method=method)
   #dev.off()
   0
 }
 
 
 # Example
-n <- 100
-M1 <- rnorm(n,0) 
-M2 <- rnorm(n,3)
-M3 <- rnorm(n,5)
+n <- 3
+M1 <- runif(n,min=-1,max=1) 
+M2 <- runif(n,min=-1,max=1) 
+M3 <- runif(n,min=-1,max=1) 
 dat <- cbind(M1,M2,M3)
-C <- corMatrix(dat)
-Corrplot_R(dat, 1, 2)
+Corrplot_R(dat, "number", 2)
 
 library(corrplot)
 M <- cor(mtcars)
-corrplot(M, method = "circle")
+corrplot(M, method = "number")
