bc80abe4dd0a691694fd7218a987de800dc45bde
Artur Tarassow <artur.tarassow@wiso.uni-hamburg.de>
2018-03-14 14:14:05 +0100

    Add option to set font size

--- R_ex.R
+++ R_ex.R
@@ -1,22 +1,44 @@
-Corrplot_R <- function(M, method, fname) {
+Corrplot_R <- function(M, method, cmin, cmax, fontsize, fname) {
+  
+  # URL: https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+  pkgTest("corrplot")
   library(corrplot)
   
   #dev.off()	# avoid permante flashing-up when using gretl
-  #png(filename=fname, type="cairo", width=600, height=600, pointsize=14, bg=NA, res=96)
-  corrplot(M, method=method)
-  #dev.off()
+  png(filename=fname, type="cairo", width=600, height=600, pointsize=fontsize, bg=NA, res=96)	
+  corrplot(M, method=method, cl.lim=c(cmin,cmax)) # ,col = gray.colors(10))
+  dev.off()
   0
 }
 
+# Corrplot_R1 <- function(M, method, fname) {
+#   # package Cairo 1.5-9 (2015-09-26)
+#   library(Cairo)
+#   library(corrplot)
+#   
+#   #dev.off()
+#   # dimensions in pixels
+#   CairoPNG(filename=fname, width=600, height=600, pointsize=14)
+#   corrplot(M, method=method)
+#   dev.off()
+#   0
+# }
+
+# check whether corrplot pckg. is already installed
+pkgTest <- function(x) {
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
 
 # Example
+fs = 22
 n <- 3
 M1 <- runif(n,min=-1,max=1) 
 M2 <- runif(n,min=-1,max=1) 
 M3 <- runif(n,min=-1,max=1) 
 dat <- cbind(M1,M2,M3)
-Corrplot_R(dat, "number", 2)
-
-library(corrplot)
-M <- cor(mtcars)
-corrplot(M, method = "number")
+fname = "/home/at/gretl/R_corrplot_test"
+Corrplot_R(dat, "number", -1, 1, fs, fname)
